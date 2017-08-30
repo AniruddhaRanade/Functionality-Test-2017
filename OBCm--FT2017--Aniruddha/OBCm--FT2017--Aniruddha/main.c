00dfc8b692b18afc0fca83c2e31809c111bd1690
singhalanirudh18 <singhalanirudh18@gmail.com>
2017-08-30 21:15:04 +0530

    added watch dog timer to the obc main code
    if the wdt runs out the whole code begins from beginning along with the initializations.

--- OBCm--FT2017--Aniruddha/OBCm--FT2017--Aniruddha/main.c
+++ OBCm--FT2017--Aniruddha/OBCm--FT2017--Aniruddha/main.c
@@ -26,7 +26,17 @@ int main(void)
 	while (1) 
     {
 		//Loop
-		
+		/*//Include this section to get a watchdog with timer of two seconds 
+		  //if the time taken to run the loop is less than 2 seconds then the timer will wait for two seconds and run the loop again.
+		  //Whereas if the time to run the loop is greater than two sec then WDT will reset the microcontroller and whole code will
+		  //run again along with the initializations.
+		  timer_reset_two_sec();
+		  wdt_enable(WDTO_2S);
+		  wdt_reset();
+		  //do the thing
+		  wdt_disable();
+		  timer_wait_reset();
+		*/
 		
 		
     }
